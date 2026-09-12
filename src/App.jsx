1d38de2706126d69a40dd5157d4d0208fbf70089
mohammadsaed99-hash <mohammad.saed99@gmail.com>
2026-09-12 10:39:34 +0300

    Create initial App component with header and description

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>FamilyTrack</h1>
+      <p>Family safety and real-time location tracking</p>
+    </div>
+  )
+}
+
+export default App
