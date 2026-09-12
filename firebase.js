3fdde9eef8e4c8c692900ca08a90b031495f51e6
mohammadsaed99-hash <mohammad.saed99@gmail.com>
2026-09-12 12:38:45 +0300

    Update Firebase configuration placeholders

--- firebase.js
+++ firebase.js
@@ -1,10 +1,16 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCKtz_9__6kCwyrh-Mhh5wD06zW_6_L-64",
+  apiKey: "ضع قيمة apiKey من Firebase هنا",
   authDomain: "familytrack-c42bf.firebaseapp.com",
   projectId: "familytrack-c42bf",
   storageBucket: "familytrack-c42bf.firebasestorage.app",
   messagingSenderId: "368013105130",
-  appId: "1:368013105130:web:de19e4853b02d362ec4493",
-  measurementId: "G-JSV6XZ5X6Z"
+  appId: "ضع قيمة appId من Firebase هنا"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export default app;
