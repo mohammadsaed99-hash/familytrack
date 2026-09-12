5244290c2e1dac60c0831dba83390514e9672901
mohammadsaed99-hash <mohammad.saed99@gmail.com>
2026-09-12 12:30:40 +0300

    Update Firebase configuration with new credentials
    Updated Firebase configuration with new credentials.

--- firebase.js
+++ firebase.js
@@ -1,16 +1,10 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "الصق apiKey هنا",
-  authDomain: "الصق authDomain هنا",
-  projectId: "الصق projectId هنا",
-  storageBucket: "الصق storageBucket هنا",
-  messagingSenderId: "الصق messagingSenderId هنا",
-  appId: "الصق appId هنا"
+  apiKey: "AIzaSyCKtz_9__6kCwyrh-Mhh5wD06zW_6_L-64",
+  authDomain: "familytrack-c42bf.firebaseapp.com",
+  projectId: "familytrack-c42bf",
+  storageBucket: "familytrack-c42bf.firebasestorage.app",
+  messagingSenderId: "368013105130",
+  appId: "1:368013105130:web:de19e4853b02d362ec4493",
+  measurementId: "G-JSV6XZ5X6Z"
 };
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export default app;
