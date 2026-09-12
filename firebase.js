d938754bc7270bcaa4c7e02bc5569de2de0aadda
mohammadsaed99-hash <mohammad.saed99@gmail.com>
2026-09-12 12:17:36 +0300

    Replace Firebase config values with placeholders
    Updated Firebase configuration with placeholders for sensitive information.

--- firebase.js
+++ firebase.js
@@ -1,10 +1,16 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCKtz_9__6kCwyrh-Mhh5wD06zW_6_L-64",
-  authDomain: "familytrack-c42bf.firebaseapp.com",
-  projectId: "familytrack-c42bf",
-  storageBucket: "familytrack-c42bf.firebasestorage.app",
-  messagingSenderId: "368013105130",
-  appId: "1:368013105130:web:de19e4853b02d362ec4493",
-  measurementId: "G-JSV6XZ5X6Z"
+  apiKey: "الصق apiKey هنا",
+  authDomain: "الصق authDomain هنا",
+  projectId: "الصق projectId هنا",
+  storageBucket: "الصق storageBucket هنا",
+  messagingSenderId: "الصق messagingSenderId هنا",
+  appId: "الصق appId هنا"
 };
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export default app;
