9b7cae909857080f9a53b5b37277adeee8e32a45
mohammadsaed99-hash <mohammad.saed99@gmail.com>
2026-09-12 11:30:24 +0300

    Refactor main.jsx to import App component
    Removed the App function definition and imported it from App.js.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-function App() {
-  return (
-    <div>
-      <h1>FamilyTrack</h1>
-      <p>Family safety and real-time location tracking</p>
-    </div>
-  )
-}
+import App from './App'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
