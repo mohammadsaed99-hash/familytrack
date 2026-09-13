e86e611a4cb08a8e7156baba44391047e76ba27e
mohammadsaed99-hash <mohammad.saed99@gmail.com>
2026-09-13 11:05:50 +0300

    Refactor Firebase messaging service to use compat libraries

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,19 +1,26 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+importScripts("https://www.gstatic.com/firebasejs/12.2.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.2.1/firebase-messaging-compat.js");
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCKtz9_9kCwyrh-Mhh5wD06zW_6L-64",
+firebase.initializeApp({
+  apiKey: "AIzaSyCKtz9_6kCwyrh-Mhh5wD06zW_6L-64",
   authDomain: "familytrack-c42bf.firebaseapp.com",
   projectId: "familytrack-c42bf",
   storageBucket: "familytrack-c42bf.firebasestorage.app",
   messagingSenderId: "368013105130",
   appId: "1:368013105130:web:de19e4853b02d362ec4493"
-};
+});
 
-const app = initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+messaging.onBackgroundMessage((payload) => {
+  const notification = payload.notification || {};
 
-export default app;
+  self.registration.showNotification(
+    notification.title || "FamilyTrack",
+    {
+      body:
+        notification.body ||
+        "You have a new FamilyTrack alert."
+    }
+  );
+});
