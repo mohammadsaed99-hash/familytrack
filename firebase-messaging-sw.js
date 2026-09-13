ed37310adafbf8d9a4f891169c22800cb1b72300
mohammadsaed99-hash <mohammad.saed99@gmail.com>
2026-09-13 10:34:38 +0300

    Add Firebase messaging service worker setup

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts("https://www.gstatic.com/firebasejs/12.2.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.2.1/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCKtz_9__6kCwyrh-Mhh5wD06zW_6L-64",
+  authDomain: "familytrack-c42bf.firebaseapp.com",
+  projectId: "familytrack-c42bf",
+  storageBucket: "familytrack-c42bf.firebasestorage.app",
+  messagingSenderId: "368013105130",
+  appId: "1:368013105130:web:de19e4853b02d362ec4493"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  const notification = payload.notification || {};
+
+  self.registration.showNotification(
+    notification.title || "FamilyTrack",
+    {
+      body: notification.body || "You have a new FamilyTrack alert.",
+      icon: "/familytrack/icon-192.png"
+    }
+  );
+});
