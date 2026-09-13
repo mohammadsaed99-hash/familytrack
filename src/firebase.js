1d93033117a9c90968b00d0cfe45876c58105570
mohammadsaed99-hash <mohammad.saed99@gmail.com>
2026-09-13 08:01:23 +0300

    Update Firebase configuration with actual keys

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "ضع قيمة apiKey من Firebase هنا",
+  apiKey: "AIzaSyCKtz9__6kCwyrh-Mhh5wD06zW_6_L-64",
   authDomain: "familytrack-c42bf.firebaseapp.com",
   projectId: "familytrack-c42bf",
   storageBucket: "familytrack-c42bf.firebasestorage.app",
   messagingSenderId: "368013105130",
-  appId: "ضع قيمة appId من Firebase هنا"
+  appId: "1:368013105130:web:de19e4853b02d362ec4493"
 };
 
 const app = initializeApp(firebaseConfig);
