65877bbc8391471e09c72d17d38ae6b13ce6ba53
mohammadsaed99-hash <mohammad.saed99@gmail.com>
2026-09-13 10:37:13 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,25 +1,26 @@
-importScripts("https://www.gstatic.com/firebasejs/12.2.1/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/12.2.1/firebase-messaging-compat.js");
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getMessaging, isSupported } from "firebase/messaging";
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCKtz_9__6kCwyrh-Mhh5wD06zW_6L-64",
+const firebaseConfig = {
+  apiKey: "AIzaSyCKtz9__6kCwyrh-Mhh5wD06zW_6L-64",
   authDomain: "familytrack-c42bf.firebaseapp.com",
   projectId: "familytrack-c42bf",
   storageBucket: "familytrack-c42bf.firebasestorage.app",
   messagingSenderId: "368013105130",
   appId: "1:368013105130:web:de19e4853b02d362ec4493"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export const messagingSupported = isSupported();
+export const messaging = messagingSupported.then((supported) => {
+  if (!supported) return null;
+  return getMessaging(app);
 });
 
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  const notification = payload.notification || {};
-
-  self.registration.showNotification(
-    notification.title || "FamilyTrack",
-    {
-      body: notification.body || "You have a new FamilyTrack alert.",
-      icon: "/familytrack/icon-192.png"
-    }
-  );
-});
+export default app;
